80c2d91a8ae65b1f40b82a0d27f213d3abb1084f
ossilv01 <ossilv01@ucm.es>
2025-02-25 22:08:34 +0100

    ll

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,12 @@
 //---------------------------------------------------------
-// Breve descripción del contenido del archivo
-// Responsable de la creación de este archivo
-// Nombre del juego
+// Breve descripción del contenido del archivo: 
+// Responsable de la creación de este archivo: Óscar Silva Urbina
+// Nombre del juego: I'm losing it
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 // Añadir aquí el resto de directivas using
 
 
@@ -17,6 +18,10 @@ public class PlayerMovement : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
     // Documentar cada atributo que aparece aquí.
     // El convenio de nombres de Unity recomienda que los atributos
     // públicos y de inspector se nombren en formato PascalCase
@@ -24,9 +29,13 @@ public class PlayerMovement : MonoBehaviour
     // Ejemplo: MaxHealthPoints
 
     #endregion
-    
+
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
+    private bool isMoving = true;
+    private Vector2 moveInput;
+    private Vector2 dir { get; set; }
+    private Vector2 lastDir { get; set; }
     // Documentar cada atributo que aparece aquí.
     // El convenio de nombres de Unity recomienda que los atributos
     // privados se nombren en formato _camelCase (comienza con _, 
@@ -35,21 +44,23 @@ public class PlayerMovement : MonoBehaviour
     // Ejemplo: _maxHealthPoints
 
     #endregion
-    
+
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
     #region Métodos de MonoBehaviour
-    
+
     // Por defecto están los típicos (Update y Start) pero:
     // - Hay que añadir todos los que sean necesarios
     // - Hay que borrar los que no se usen 
-    
+
     /// <summary>
     /// Start is called on the frame when a script is enabled just before 
     /// any of the Update methods are called the first time.
     /// </summary>
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        dir = lastDir = Vector2.zero;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     /// <summary>
@@ -57,7 +68,10 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     void Update()
     {
-        
+       if (isMoving)
+        {
+            dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
     }
     #endregion
 
@@ -70,9 +84,31 @@ public class PlayerMovement : MonoBehaviour
     // Ejemplo: GetPlayerController
 
     #endregion
-    
+
     // ---- MÉTODOS PRIVADOS ----
     #region Métodos Privados
+    public Vector2 getLastDir()
+    {
+        return lastDir;
+    }
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Detectar si el jugador está sobre una plataforma en movimiento
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            platform = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Salir de la plataforma
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            platform = null;
+        }
+    }
+    */
     // Documentar cada método que aparece aquí
     // El convenio de nombres de Unity recomienda que estos métodos
     // se nombren en formato PascalCase (palabras con primera letra
