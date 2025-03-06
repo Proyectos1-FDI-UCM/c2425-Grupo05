f6c4678b8f704a5f2a70ba4e411574757833949b
Bucerillo <antobuce@ucm.es>
2025-03-06 23:02:21 +0100

    Se resetea el RoomTime cuando muere el player

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -174,6 +174,8 @@ public class LevelManager : MonoBehaviour
         }
         StateTime = 0;
         State = 0;
+        RoomTimeRemaining = RoomMaxTime;
+
 
     }
     public void ChangeState()
